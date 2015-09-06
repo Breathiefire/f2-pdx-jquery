03a9b9787502a7e09b43308dd3734c6dbfe85262
Beth Adele Long <beth@virtualcreatives.com>
2015-09-05 19:20:46 -0700

    add examples and notes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,24 @@
-// Add your JS code here
-
 //
 // Example 1: getElementsByTagName()
 //
+// Vanilla JS Task:
+//        Using JavaScript, add a descriptive page title.
 var titleTags = document.getElementsByTagName("title");
 var title = titleTags[0]; //titleTags is an array
 title.text = "F2: JavaScript | Class Summary";
 
-
 //
 // Example 2: getElementById()
 //
-// Task: Use getElementById to add a style attribute to the <ul>
+// Vanilla JS Task:
+//       Use getElementById to add a style attribute to the <ul>
 //       that removes the bullets to the left of the images.
 //
 // Resources:
 // https://developer.mozilla.org/en-US/docs/Web/API/Element.setAttribute
 // https://developer.mozilla.org/en-US/docs/Web/CSS/list-style-type
 //
-var picsUl = document.getElementById("lab-images");
-picsUl.style.cssText = "list-style-type: none;";
+
 
 // Alternate:
 // picsUl.setAttribute("style", "list-style-type: none;");
@@ -27,30 +26,72 @@ picsUl.style.cssText = "list-style-type: none;";
 //
 // Example 3: querySelectorAll()
 //
-// Task: Make the images all have a maximum width of 200px.
+// Vanilla JS Task:
+//       Make the images all have a maximum width of 200px.
 //
-var imgTags = document.querySelectorAll("li img");
 
-for(var i = 0; i < imgTags.length; i++) {
-  imgTags[i].setAttribute(
-    "style",
-    "max-width: 200px;"
-  );
-}
 
 //
-// Task: Find another picture to illustrate one of the labs.
+// Example 4: querySelector() + createElement() + appendChild()
+//
+// Vanilla JS Task:
+//       Find another picture to illustrate one of the labs.
 //       Use JavaScript to add a new <li> element in your existing <ul>.
 //       Then add a new <img> element to the <li> and specify its src.
 //       You can use the URL of the image you found as the src,
 //       or you can download the new image to your assets/img folder.
 //
-// You’ll need document.createElement() and element.appendChild()
+// You’ll need: document.createElement() and element.appendChild()
 //
 
-var ulTag = document.querySelector("ul"); // Note: this assumes just one ul on the page
-var newLi = document.createElement("li");
-var newImg = document.createElement("img");
-newImg.setAttribute( "src", "http://www.ex-astris-scientia.org/gallery/30-jahre/st3-klingons.jpg" );
-newLi.appendChild(newImg);
-ulTag.appendChild(newLi); // Why doesn't it have the styling we applied earlier?
+
+//
+// Example 5: window.addEventListener("load")
+//
+// Vanilla JS Task:
+//        Open an alert dialog on page load to welcome the user to the page.
+//
+
+//
+// Example 6: Handling the resize event
+//
+//            resize = An event on the window object.
+//            Signifies the user changed the viewing dimensions.
+//            Use it to implement "responsive design": adjust layout, styling, and element visibility.
+//
+// Vanilla JS Task:
+//        Make the images half the window width each time the resize event triggers.
+//
+// You’ll need: window.innerWidth and window.innerHeight
+//
+
+
+//
+// jQuery Task:
+//        Repeat the previous task, this time using jQuery.
+
+
+//
+// Example 7: Handling a click event
+//
+//            click = An event on a DOM element.
+//            Signifies the user clicked that element with a mouse or trackpad.
+//            Use it to interact with the user: open a menu,respond to a button-click, etc.
+//
+// Vanilla JS Task:
+//        Every time an image is clicked, move it to the end of the <ul>.
+//
+// You’ll need: event.target.parentNode
+//              element.remove()
+//              element.appendChild()
+//
+
+//
+// jQuery Task:
+//        Repeat the previous task, this time using jQuery.
+
+//
+// jQuery Task:
+//       Hide all the lab images on page load.
+//       When a button is clicked, show the corresponding list item (and only that list item)
+//
